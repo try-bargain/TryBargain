2061b1316bb3efc9f72c65a6e35ed8595490c970
hoho <ghdnjs18@naver.com>
2024-01-10 18:22:38 +0900

    git init

---
+++ src/main/java/com/project/trybargain/TryBargainApplication.java
@@ -0,0 +1,13 @@
+package com.project.trybargain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryBargainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryBargainApplication.class, args);
+    }
+
+}
