e665a49085dd1af0c3ca636f254e5ba093e66452
ghdnjs18 <ghdnjs18@naver.com>
2024-01-16 00:34:56 +0900

    Refactor: Board 순환 참조 수정

--- src/main/java/com/project/trybargain/domain/board/dto/BoardResponseDto.java
+++ src/main/java/com/project/trybargain/domain/board/dto/BoardResponseDto.java
@@ -2,12 +2,11 @@ package com.project.trybargain.domain.board.dto;
 
 import com.project.trybargain.domain.board.entity.Board;
 import com.project.trybargain.domain.board.entity.Category;
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotNull;
-import org.hibernate.annotations.ColumnDefault;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class BoardResponseDto {
     private Long id;
     private String title;
@@ -15,9 +14,8 @@ public class BoardResponseDto {
     private int price;
     private int board_like;
     private boolean active_yn;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private LocalDateTime deletedAt;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
     private Category category;
 
     public BoardResponseDto(Board board) {
@@ -27,8 +25,8 @@ public class BoardResponseDto {
         this.price = board.getPrice();
         this.board_like = board.getBoard_like();
         this.active_yn = board.isActive_yn();
-        this.createdAt = board.getCreated_at();
-        this.updatedAt = board.getUpdated_at();
+        this.created_at = board.getCreated_at();
+        this.updated_at = board.getUpdated_at();
         this.category = board.getCategory();
     }
 }
