f46ec50f6d4bd1921f4f5d7c3080fcfcf4f3e5e7
wooseok <shdntjrrj@naver.com>
2024-01-17 18:03:45 +0900

    feat : 마이페이지 및 회원정보 수정기능 구현

--- src/main/java/com/project/trybargain/domain/user/service/UserService.java
+++ src/main/java/com/project/trybargain/domain/user/service/UserService.java
@@ -1,12 +1,13 @@
 package com.project.trybargain.domain.user.service;
 
 import com.project.trybargain.domain.user.dto.JoinRequestDto;
+import com.project.trybargain.domain.user.dto.MyPageResponseDto;
+import com.project.trybargain.domain.user.dto.UpdateMyPageRequestDto;
 import com.project.trybargain.domain.user.entity.User;
 import com.project.trybargain.domain.user.entity.UserInfo;
 import com.project.trybargain.domain.user.repository.UserRepository;
 import com.project.trybargain.global.dto.MessageResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,7 +26,6 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     //회원 가입
-    @Transactional
     public ResponseEntity<MessageResponseDto> join(JoinRequestDto joinRequestDto) {
         validateCheckUser(joinRequestDto.getUser_id());
 
@@ -44,15 +44,34 @@ public class UserService {
 
     //회원가입 검증
     private void validateCheckUser(String userId) {
-
         Optional<User> findUser = userRepository.findUserByUserId(userId);
 
         if(!findUser.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 아이디입니다.");
         }
 
-
     }
+
+    //마이페이지 조회
+    public MyPageResponseDto getMyInfo(long id) {
+        User user = findUser(id);
+        return new MyPageResponseDto(user);
+    }
+
+    //사용자 존재 체크
+    private User findUser(long id) {
+        return userRepository.findById(id).orElseThrow(() ->
+                new NullPointerException("해당 유저는 존재하지 않습니다."));
+    }
+
+    public ResponseEntity<MessageResponseDto> updateUser(UpdateMyPageRequestDto requestDto, long id) {
+        User updateUser = findUser(id);
+        updateUser.update(requestDto);
+
+        MessageResponseDto responseEntity = new MessageResponseDto("update complete",200);
+        return ResponseEntity.status(HttpStatus.OK).body(responseEntity);
+    }
+
 
 
 }
