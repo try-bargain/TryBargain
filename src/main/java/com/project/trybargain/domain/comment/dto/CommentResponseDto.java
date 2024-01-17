904ea5d8e1f3625a4dc71df92fd04e1222e225ce
hoho <ghdnjs18@naver.com>
2024-01-17 12:30:29 +0900

    feat: Comment API 틀잡기

---
+++ src/main/java/com/project/trybargain/domain/comment/dto/CommentResponseDto.java
@@ -0,0 +1,4 @@
+package com.project.trybargain.domain.comment.dto;
+
+public class CommentResponseDto {
+}
---
+++ src/main/java/com/project/trybargain/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.project.trybargain.domain.comment.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepository {
+}
