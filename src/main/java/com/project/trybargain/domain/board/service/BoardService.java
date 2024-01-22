69bf29b9ce9eeefe8796c439c9162273a357a104
hoho <ghdnjs18@naver.com>
2024-01-22 15:56:19 +0900

    feat: 게시글 좋아요 기능 구현

--- src/main/java/com/project/trybargain/domain/board/service/BoardService.java
+++ src/main/java/com/project/trybargain/domain/board/service/BoardService.java
@@ -3,7 +3,9 @@ package com.project.trybargain.domain.board.service;
 import com.project.trybargain.domain.board.dto.BoardRequestDto;
 import com.project.trybargain.domain.board.dto.BoardResponseDto;
 import com.project.trybargain.domain.board.entity.Board;
+import com.project.trybargain.domain.board.entity.BoardLike;
 import com.project.trybargain.domain.board.entity.Category;
+import com.project.trybargain.domain.board.repository.BoardLikeRepository;
 import com.project.trybargain.domain.board.repository.BoardRepository;
 import com.project.trybargain.domain.board.repository.CategoryRepository;
 import com.project.trybargain.domain.user.entity.User;
@@ -26,6 +28,7 @@ public class BoardService {
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
     private final CategoryRepository categoryRepository;
+    private final BoardLikeRepository boardLikeRepository;
 
     // 게시글 등록
     @Transactional
@@ -83,6 +86,31 @@ public class BoardService {
         board.delete();
 
         MessageResponseDto responseEntity = new MessageResponseDto("게시글 삭제를 성공하였습니다.",200);
+        return ResponseEntity.status(HttpStatus.OK).body(responseEntity);
+    }
+
+    // 게시글 좋아요
+    @Transactional
+    public ResponseEntity<MessageResponseDto> likeBoard(long id, long userId) {
+        Board board = findBoard(id);
+        User user = findUser(userId);
+
+        BoardLike boardLike = boardLikeRepository.findByUserAndBoard(user, board).orElse(null);
+        if (boardLike == null) {
+            boardLike = new BoardLike(user, board);
+            boardLikeRepository.save(boardLike);
+        }
+
+        if (boardLike.isLike_yn()) {
+            board.changeLike(board.getBoard_like() - 1);
+            boardLike.changeLike();
+            MessageResponseDto responseEntity = new MessageResponseDto("게시글 좋아요를 취소하였습니다.",200);
+            return ResponseEntity.status(HttpStatus.OK).body(responseEntity);
+        }
+
+        board.changeLike(board.getBoard_like() + 1);
+        boardLike.changeLike();
+        MessageResponseDto responseEntity = new MessageResponseDto("게시글 좋아요를 성공하였습니다.",200);
         return ResponseEntity.status(HttpStatus.OK).body(responseEntity);
     }
 
