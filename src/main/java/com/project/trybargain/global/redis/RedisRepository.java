2f9e339bdda06003b30daf7947e32a3773368522
hoho <ghdnjs18@naver.com>
2024-02-19 18:20:25 +0900

    refector: 게시글 좋아요 redis 사용으로 변경

---
+++ src/main/java/com/project/trybargain/global/redis/RedisRepository.java
@@ -0,0 +1,39 @@
+package com.project.trybargain.global.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisRepository {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String key, String value) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value);
+    }
+
+    public void saveSet(String key, String value) {
+        SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
+        setOperations.add(key, value);
+    }
+
+    public String getValue(String key) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        return (String) valueOperations.get(key);
+    }
+
+    public Set<Object> getSetValues(String key) {
+        SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
+        return setOperations.members(key);
+    }
+
+    public void deleteSetValue(String key, String value) {
+        SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
+        setOperations.remove(key, value);
+    }
+}
